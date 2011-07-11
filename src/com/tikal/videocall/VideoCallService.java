1a45fd386e1ca0dccc17acf7fd0d6dd1dcc13e44
rbenitez <rbenitez@07af3ef0-874b-4d75-9f19-b07be4c0a58b>
2011-07-11 16:01:43 +0000

    Add notify to regain the active call.

--- src/com/tikal/videocall/VideoCallService.java
+++ src/com/tikal/videocall/VideoCallService.java
@@ -17,8 +17,13 @@ import com.tikal.media.AudioInfo;
 import com.tikal.media.VideoInfo;
 import com.tikal.sip.Controller;
 import com.tikal.softphone.R;
+import com.tikal.softphone.SoftPhone;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
@@ -29,6 +34,15 @@ public class VideoCallService extends Service {
 
 	AudioMediaGroup audioMediaGroup = null;
 	VideoMediaGroup videoMediaGroup = null;
+	
+
+	private NotificationManager mNotificationMgr;
+	private final static int NOTIF_ID = 2;
+	
+	private Notification mNotif;
+	private PendingIntent mNotifContentIntent;
+	private Intent notifIntent;
+	private String notificationTitle = "VideoCall";
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -40,10 +54,27 @@ public class VideoCallService extends Service {
 		super.onCreate();
 		// Crear mediagroups y registralos en el context
 
+		/* Notification Video Call Active */
+
+		mNotificationMgr = (NotificationManager) this
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		mNotif = new Notification(R.drawable.ic_jog_dial_answer, notificationTitle,
+				System.currentTimeMillis());
+		mNotif.flags |= Notification.FLAG_ONGOING_EVENT;
+
+		notifIntent = new Intent(this, VideoCall.class);
+		mNotifContentIntent = PendingIntent
+				.getActivity(this, 0, notifIntent, 0);
+		mNotif.setLatestEventInfo(this, notificationTitle, "",
+				mNotifContentIntent);
+
+		mNotificationMgr.notify(NOTIF_ID, mNotif);
+
 		Controller controller = (Controller) ApplicationContext.contextTable
 				.get("controller");
-		
-//FIXME controller != null
+
+		// FIXME controller != null
 		NetworkConnectionImpl nc = controller.getNetworkConnection();
 
 		VideoInfo vi = nc.getVideoInfo();
@@ -52,29 +83,29 @@ public class VideoCallService extends Service {
 		String sdpVideo = nc.getSdpVideo();
 		String sdpAudio = nc.getSdpAudio();
 
-//		if (!sdpAudio.equals("")) {
-			try {
-				audioMediaGroup = new AudioMediaGroup(
-						MediaGroup.PLAYER_RECORDER_SIGNALDETECTOR,
-						ai.getSample_rate(), ai.getFrameSize());
-			} catch (MsControlException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-//		}
+		// if (!sdpAudio.equals("")) {
+		try {
+			audioMediaGroup = new AudioMediaGroup(
+					MediaGroup.PLAYER_RECORDER_SIGNALDETECTOR,
+					ai.getSample_rate(), ai.getFrameSize());
+		} catch (MsControlException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// }
 		Log.d(LOG_TAG, "sdpVideo:\n" + sdpVideo);
-//		if (!sdpVideo.equals("")) {
-			Log.d(LOG_TAG, "create videoMediaGroup");
-			try {
-				videoMediaGroup = new VideoMediaGroup(
-						MediaGroup.PLAYER_RECORDER_SIGNALDETECTOR, null,
-						vi.getWidth(), vi.getHeight(), null, 0, 0);
-			} catch (MsControlException e) {
-				// TODO Auto-generated catch block
-				Log.e(LOG_TAG, "Error on create videoMediaGroup");
-				e.printStackTrace();
-			}
-//		}
+		// if (!sdpVideo.equals("")) {
+		Log.d(LOG_TAG, "create videoMediaGroup");
+		try {
+			videoMediaGroup = new VideoMediaGroup(
+					MediaGroup.PLAYER_RECORDER_SIGNALDETECTOR, null,
+					vi.getWidth(), vi.getHeight(), null, 0, 0);
+		} catch (MsControlException e) {
+			// TODO Auto-generated catch block
+			Log.e(LOG_TAG, "Error on create videoMediaGroup");
+			e.printStackTrace();
+		}
+		// }
 
 		Log.d(LOG_TAG, "videoMediaGroup: " + videoMediaGroup);
 		ApplicationContext.contextTable.put("audioMediaGroup", audioMediaGroup);
@@ -112,8 +143,8 @@ public class VideoCallService extends Service {
 						nc.getJoinableStream(StreamType.video));
 				videoMediaGroup.getPlayer().play(URI.create(""), RTC.NO_RTC,
 						Parameters.NO_PARAMETER);
-				 videoMediaGroup.getRecorder().record(URI.create(""),
-				 RTC.NO_RTC, Parameters.NO_PARAMETER);
+				videoMediaGroup.getRecorder().record(URI.create(""),
+						RTC.NO_RTC, Parameters.NO_PARAMETER);
 
 				// Unjoin example
 				// videoMediaGroup.stop();
@@ -139,6 +170,8 @@ public class VideoCallService extends Service {
 
 		if (videoMediaGroup != null)
 			videoMediaGroup.stop();
+		
+		mNotificationMgr.cancel(NOTIF_ID);
 		super.onDestroy();
 	}
 
