d06a1ed98eccabcb8c144d6f9c3c8b36b24e0952
mparis <mparis@07af3ef0-874b-4d75-9f19-b07be4c0a58b>
2011-07-13 08:00:54 +0000

    Permit stream type change.

--- src/com/tikal/javax/media/mscontrol/mediagroup/AudioRecorder.java
+++ src/com/tikal/javax/media/mscontrol/mediagroup/AudioRecorder.java
@@ -11,7 +11,6 @@ import android.media.AudioTrack;
 import android.util.Log;
 
 import com.tikal.android.media.rx.AudioRx;
-import com.tikal.android.media.rx.MediaRx;
 
 public class AudioRecorder extends RecorderBase implements AudioRx {
 
@@ -23,56 +22,70 @@ public class AudioRecorder extends RecorderBase implements AudioRx {
 	private AudioTrack audioTrack;
 	private int buffer_min;
 	private int streamType;
-	
+
 	private boolean isRecording = false;
 
-	public AudioRecorder(MediaGroupBase parent, int sampleRate,
-			int streamType) throws MsControlException {
-		super(parent);
-		
-		frequency = sampleRate;
-		Log.d(LOG_TAG, "Frequency = " + frequency);
-		buffer_min = AudioTrack.getMinBufferSize(frequency,
-				channelConfiguration, audioEncoding);
-		
-		audioTrack = new AudioTrack(streamType, frequency,
+	public synchronized int getStreamType() {
+		return streamType;
+	}
+
+	public synchronized void setStreamType(int streamType) {
+		this.streamType = streamType;
+		isRecording = false;
+		if (audioTrack != null)
+			audioTrack.release();
+		audioTrack = new AudioTrack(this.streamType, frequency,
 				channelConfiguration, audioEncoding, buffer_min,
 				AudioTrack.MODE_STREAM);
 	}
 
-	public void release() {
-		Log.d(LOG_TAG, "Release");
-		MediaRx.stopAudioRx();
-		if (audioTrack != null)
-			audioTrack.release();
+	public boolean isRecording() {
+		return isRecording;
 	}
 
-	public void setStreamType(int streamType) {
+	public AudioRecorder(MediaGroupBase parent, int sampleRate, int streamType)
+			throws MsControlException {
+		super(parent);
+
+		frequency = sampleRate;
+		Log.d(LOG_TAG, "Frequency = " + frequency);
 		this.streamType = streamType;
+		buffer_min = AudioTrack.getMinBufferSize(frequency,
+				channelConfiguration, audioEncoding);
 
+		audioTrack = new AudioTrack(this.streamType, frequency,
+				channelConfiguration, audioEncoding, buffer_min,
+				AudioTrack.MODE_STREAM);
 	}
 
-	public int getStreamType() {
-		return streamType;
-	}
+	// public void release() {
+	// Log.d(LOG_TAG, "Release");
+	// // MediaRx.stopAudioRx();
+	// if (audioTrack != null)
+	// audioTrack.release();
+	// }
 
 	@Override
-	public void putAudioSamplesRx(byte[] audio, int length) {
+	public synchronized void putAudioSamplesRx(byte[] audio, int length) {
 		if (isRecording && audioTrack != null)
 			audioTrack.write(audio, 0, length);
 	}
-	
+
 	@Override
-	public void record(URI arg0, RTC[] arg1, Parameters arg2)
+	public synchronized void record(URI arg0, RTC[] arg1, Parameters arg2)
 			throws MsControlException {
-		audioTrack.play();
-		isRecording = true;
+		if (audioTrack != null) {
+			audioTrack.play();
+			isRecording = true;
+		}
 	}
 
 	@Override
-	public void stop() {
-		audioTrack.stop();
-		isRecording = false;
+	public synchronized void stop() {
+		if (audioTrack != null) {
+			audioTrack.stop();
+			isRecording = false;
+		}
 	}
 
 }
