48c6dd7c22eca4130b9ea990ea1b3bbf9d25d023
Raúl Benítez <rbenitez@gsyc.es>
2011-09-29 10:49:44 +0200

    Add license headers

--- src/com/kurento/kas/phone/softphone/SoftPhone.java
+++ src/com/kurento/kas/phone/softphone/SoftPhone.java
@@ -1,3 +1,19 @@
+/*
+Softphone application for Android. It can make video calls using SIP with different video formats and audio formats.
+Copyright (C) 2011 Tikal Technologies
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.kurento.kas.phone.softphone;
 
 import java.net.InetAddress;
