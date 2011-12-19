0825b3893546ae33fc1a75a19ce19ac36b54df1f
Raúl Benítez <rbenitez@gsyc.es>
2011-12-19 11:07:49 +0100

    Add control to controllate when you receive a INVITE and you send other INVITE. Only one call progresses

--- src/com/kurento/kas/phone/sip/Controller.java
+++ src/com/kurento/kas/phone/sip/Controller.java
@@ -20,7 +20,8 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Map;
 
-import android.hardware.Camera;
+import javax.sql.rowset.spi.SyncResolver;
+
 import android.util.Log;
 
 import com.kurento.commons.mscontrol.Parameters;
@@ -57,9 +58,21 @@ public class Controller implements SipEndPointListener, SipCallListener,
 	private SipCall currentCall;
 	private SipCall incomingCall;
 
+	private Boolean isCall;
+
 	private CallListener callListener;
 
 	private MediaSessionAndroid mediaSession;
+
+	private synchronized void setIsCall(Boolean isCall) {
+		this.isCall = isCall;
+		Log.w(LOG_TAG, "--- setIsCall -> " + this.isCall );
+	}
+
+	private synchronized Boolean getIsCall() {
+		Log.w(LOG_TAG, "--- getIsCall -> " + this.isCall );
+		return this.isCall;
+	}
 
 	public UA getUa() {
 		return ua;
@@ -144,6 +157,7 @@ public class Controller implements SipEndPointListener, SipCallListener,
 		}
 
 		register(localUser, localPassword, localRealm);
+		setIsCall(false);
 	}
 
 	public boolean isRegister() {
@@ -174,7 +188,6 @@ public class Controller implements SipEndPointListener, SipCallListener,
 			incomingCall.addListener(this);
 
 			try {
-
 				String cad = "";
 				for (MediaType mediaType : event.getCallSource()
 						.getMediaTypesModes().keySet()) {
@@ -183,20 +196,39 @@ public class Controller implements SipEndPointListener, SipCallListener,
 							+ event.getCallSource().getMediaTypesModes()
 									.get(mediaType) + "\n";
 				}
-				String getCallSource = (String) ApplicationContext.contextTable
-						.get("getCallSource");
 
-				if (getCallSource == null) {
-					ApplicationContext.contextTable.put("getCallSource", event
-							.getCallSource().toString());
+				if (getIsCall()) {
+					Log.d(LOG_TAG,
+							"Send reject because I've received an INVITE");
+					reject();
+				} else {
+					setIsCall(true);
+					Log.d(LOG_TAG, "I've received an INVITE");
 					Log.d(LOG_TAG, "Me llama Uri: "
 							+ event.getCallSource().getRemoteUri());
 					if (callListener != null)
 						callListener.incomingCall(event.getCallSource()
 								.getRemoteUri());
-				} else {
-					reject();
 				}
+
+				// String getCallSource = (String)
+				// ApplicationContext.contextTable
+				// .get("getCallSource");
+				//
+				// if (getCallSource == null) {
+				// Log.d(LOG_TAG, "I've received an INVITE");
+				// ApplicationContext.contextTable.put("getCallSource", event
+				// .getCallSource().toString());
+				// Log.d(LOG_TAG, "Me llama Uri: "
+				// + event.getCallSource().getRemoteUri());
+				// if (callListener != null)
+				// callListener.incomingCall(event.getCallSource()
+				// .getRemoteUri());
+				// } else {
+				// Log.d(LOG_TAG,
+				// "Send reject because I've received an INVITE");
+				// reject();
+				// }
 			} catch (Exception e) {
 				Log.e(LOG_TAG, e.toString());
 				e.printStackTrace();
@@ -236,6 +268,7 @@ public class Controller implements SipEndPointListener, SipCallListener,
 
 		if (SipCallEvent.CALL_SETUP.equals(eventType)) {
 			currentCall = event.getSource();
+			setIsCall(true);
 			Log.d(LOG_TAG, "Setting currentCall");
 			if (callListener != null) {
 				ApplicationContext.contextTable.put("callDirection",
@@ -243,50 +276,86 @@ public class Controller implements SipEndPointListener, SipCallListener,
 				callListener.callSetup(currentCall.getNetworkConnection(null));
 			}
 		} else if (SipCallEvent.CALL_TERMINATE.equals(eventType)) {
+			setIsCall(false);
 			Log.d(LOG_TAG, "Call Terminate");
 			if (callListener != null)
 				callListener.callTerminate();
-			ApplicationContext.contextTable.remove("getCallSource");
+			// ApplicationContext.contextTable.remove("getCallSource");
 		} else if (SipCallEvent.CALL_REJECT.equals(eventType)) {
+			setIsCall(false);
 			Log.d(LOG_TAG, "Call Reject");
 			if (callListener != null)
 				callListener.callReject();
+
+			// ApplicationContext.contextTable.remove("getCallSource");
 		} else if (SipCallEvent.CALL_CANCEL.equals(eventType)) {
+			setIsCall(false);
 			Log.d(LOG_TAG, "Call Cancel");
 			if (callListener != null)
 				callListener.callCancel();
-			ApplicationContext.contextTable.remove("getCallSource");
+			// ApplicationContext.contextTable.remove("getCallSource");
 		} else if (SipCallEvent.CALL_ERROR.equals(eventType)) {
+			setIsCall(false);
 			Log.d(LOG_TAG, "Call Error");
 			if (callListener != null)
 				callListener.callReject();
+			// ApplicationContext.contextTable.remove("getCallSource");
 		}
 	}
 
 	@Override
 	public void aceptCall() throws Exception {
-		if (incomingCall != null)
+		if (incomingCall != null) {
+			setIsCall(true);
 			incomingCall.accept();
+		}
 	}
 
 	@Override
 	public void reject() throws Exception {
-		ApplicationContext.contextTable.remove("getCallSource");
+		Log.d(LOG_TAG, "***Send a REJECT");
+		setIsCall(false);
+		// ApplicationContext.contextTable.remove("getCallSource");
 		pendingEndPointEvent.getCallSource().reject();
 	}
 
 	@Override
 	public void call(String remoteURI) throws Exception {
-		Log.d(LOG_TAG, "calling..." + remoteURI);
-		currentCall = endPoint.dial(remoteURI, this);
+
+		if (getIsCall()) {
+			Log.d(LOG_TAG,
+					"You cannot send an INVITE because exist a Call Active");
+			callListener.callReject();// TODO: Review
+		} else {
+			setIsCall(true);
+			Log.d(LOG_TAG, "***Send an INVITE to " + remoteURI);
+			currentCall = endPoint.dial(remoteURI, this);
+
+//			ApplicationContext.contextTable.put("getCallSource",
+//					currentCall.toString());
+		}
+		// String getCallSource = (String) ApplicationContext.contextTable
+		// .get("getCallSource");
+		//
+		// if (getCallSource == null) {
+		// currentCall = endPoint.dial(remoteURI, this);
+		// Log.d(LOG_TAG, "***Send an INVITE to " + remoteURI);
+		// ApplicationContext.contextTable.put("getCallSource",
+		// currentCall.toString());
+		// } else {
+		// Log.d(LOG_TAG,
+		// "You cannot send an INVITE because exist a Call Active");
+		// callListener.callReject();// TODO: Review
+		// }
 	}
 
 	@Override
 	public void hang() {
-		Log.d(LOG_TAG, "hanging...");
+		Log.d(LOG_TAG, "***Send a HANG");
+//		ApplicationContext.contextTable.remove("getCallSource");
+		setIsCall(false);
 		if (currentCall != null)
 			try {
-				ApplicationContext.contextTable.remove("getCallSource");
 				currentCall.hangup();
 			} catch (ServerInternalErrorException e) {
 				e.printStackTrace();
@@ -295,7 +364,9 @@ public class Controller implements SipEndPointListener, SipCallListener,
 
 	@Override
 	public void cancel() {
-		Log.d(LOG_TAG, "canceling...");
+		Log.d(LOG_TAG, "***Send a CANCEL");
+//		ApplicationContext.contextTable.remove("getCallSource");
+		setIsCall(false);
 		if (currentCall != null)
 			try {
 				currentCall.cancel();
