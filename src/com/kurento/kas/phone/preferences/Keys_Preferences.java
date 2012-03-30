27690116f652c30dd3529e6a7c0ac7ec831d4452
Raúl Benítez <rbenitez@gsyc.es>
2012-03-30 16:32:29 +0200

    Add new class that they define the keys to preferences and the posibles actions

---
+++ src/com/kurento/kas/phone/preferences/Keys_Preferences.java
@@ -0,0 +1,66 @@
+package com.kurento.kas.phone.preferences;
+
+public class Keys_Preferences {
+	// Connection Preferences
+	// Sip Connection Prerefences
+	public static String SIP_LOCAL_USERNAME = "SIP_LOCAL_USERNAME";
+	public static String SIP_LOCAL_PASSWORD = "SIP_LOCAL_PASSWORD";
+	public static String SIP_LOCAL_DOMAIN = "SIP_LOCAL_DOMAIN";
+	public static String SIP_PROXY_IP = "SIP_PROXY_IP";
+	public static String SIP_PROXY_PORT = "SIP_PROXY_PORT";
+	public static String SIP_MIN_LOCAL_PORT = "SIP_MIN_LOCAL_PORT";
+	public static String SIP_MAX_LOCAL_PORT = "SIP_MAX_LOCAL_PORT";
+
+	// Push Connection Preference
+	public static String PUSH_CLIENT_USERNAME = "PUSH_CLIENT_USERNAME";
+	public static String PUSH_CLIENT_PASSWORD = "PUSH_CLIENT_PASSWORD";
+	public static String PUSH_CLIENT_DOMAIN = "PUSH_CLIENT_DOMAIN";
+	public static String PUSH_SERVER_IP = "PUSH_SERVER_IP";
+	public static String PUSH_SERVER_PORT = "PUSH_SERVER_PORT";
+
+	// Servlet Connection Preference
+	public static String SERVLET_SERVER = "SERVLET_SERVER";
+
+	// Media Preferences
+	// General Category
+	public static String MEDIA_GENERAL_MAX_BW = "MEDIA_GENERAL_MAX_BW";
+	public static String MEDIA_GENERAL_MAX_DELAY = "MEDIA_GENERAL_MAX_DELAY";
+	public static String MEDIA_GENERAL_FRONT_CAMERA = "MEDIA_GENERAL_FRONT_CAMERA";
+
+	// Network Category
+	public static String MEDIA_NET_KEEP_ALIVE = "MEDIA_NET_KEEP_ALIVE";
+	public static String MEDIA_NET_KEEP_DELAY = "MEDIA_NET_KEEP_DELAY";
+	public static String MEDIA_NET_TRANSPORT = "MEDIA_NET_TRANSPORT";
+
+	// Video Category
+	public static String MEDIA_VIDEO_CODECS = "MEDIA_VIDEO_CODECS";
+	public static String MEDIA_VIDEO_H263_CODEC = "MEDIA_VIDEO_H263_CODEC";
+	public static String MEDIA_VIDEO_MPEG4_CODEC = "MEDIA_VIDEO_MPEG4_CODEC";
+	public static String MEDIA_VIDEO_H264_CODEC = "MEDIA_VIDEO_H264_CODEC";
+	public static String MEDIA_VIDEO_LOCAL_PORT_RANGE = "MEDIA_VIDEO_LOCAL_PORT_RANGE";
+	public static String MEDIA_MIN_VIDEO_LOCAL_PORT = "MEDIA_MIN_VIDEO_LOCAL_PORT";
+	public static String MEDIA_MAX_VIDEO_LOCAL_PORT = "MEDIA_MAX_VIDEO_LOCAL_PORT";
+	public static String MEDIA_VIDEO_SIZE = "MEDIA_VIDEO_SIZE";
+	public static String MEDIA_VIDEO_CALL_DIRECTION = "MEDIA_VIDEO_CALL_DIRECTION";
+	public static String MEDIA_VIDEO_MAX_FR = "MEDIA_VIDEO_MAX_FR";
+	public static String MEDIA_VIDEO_GOP_SIZE = "MEDIA_VIDEO_GOP_SIZE";
+	public static String MEDIA_VIDEO_QUEUE_SIZE = "MEDIA_VIDEO_QUEUE_SIZE";
+
+	// Audio Category
+	public static String MEDIA_AUDIO_CODECS = "MEDIA_AUDIO_CODECS";
+	public static String MEDIA_AUDIO_AMR_CODEC = "MEDIA_AUDIO_AMR_CODEC";
+	public static String MEDIA_AUDIO_MP2_CODEC = "MEDIA_AUDIO_MP2_CODEC";
+	public static String MEDIA_AUDIO_AAC_CODEC = "MEDIA_AUDIO_AAC_COCEC";
+	public static String MEDIA_AUDIO_PCMU_CODEC = "MEDIA_AUDIO_PCMU_CODEC";
+	public static String MEDIA_AUDIO_PCMA_CODEC = "MEDIA_AUDIO_PCMA_CODEC";
+	public static String MEDIA_AUDIO_LOCAL_PORT_RANGE = "MEDIA_AUDIO_LOCAL_PORT_RANGE";
+	public static String MEDIA_MIN_AUDIO_LOCAL_PORT = "MEDIA_MIN_AUDIO_LOCAL_PORT";
+	public static String MEDIA_MAX_AUDIO_LOCAL_PORT = "MEDIA_MAX_AUDIO_LOCAL_PORT";
+	public static String MEDIA_AUDIO_CALL_DIRECTION = "MEDIA_AUDIO_CALL_DIRECTION";
+
+	// Stun
+	public static String STUN_LIST = "STUN_LIST";
+	public static String STUN_HOST = "STUN_HOST";
+	public static String STUN_HOST_PORT = "STUN_HOST_PORT";
+
+}
---
+++ src/com/kurento/kas/phone/shared/Actions.java
@@ -0,0 +1,29 @@
+package com.kurento.kas.phone.shared;
+
+import com.kurento.commons.ua.event.CallEvent;
+import com.kurento.commons.ua.event.EndPointEvent;
+
+public class Actions {
+	public final static String REGISTER_USER_SUCESSFUL = EndPointEvent.REGISTER_USER_SUCESSFUL
+			.toString();
+	public final static String REGISTER_USER_FAIL = EndPointEvent.REGISTER_USER_FAIL
+			.toString();
+	public final static String REGISTER_USER_NOT_FOUND = EndPointEvent.REGISTER_USER_NOT_FOUND
+			.toString();
+	public final static String UNREGISTER_USER_SUCESSFUL = EndPointEvent.UNREGISTER_USER_SUCESSFUL
+			.toString();
+	public final static String UNREGISTER_USER_FAIL = EndPointEvent.UNREGISTER_USER_FAIL
+			.toString();
+
+	public final static String INCOMING_CALL = EndPointEvent.INCOMING_CALL.toString();
+	public final static String OUTGOING_CALL = "OUTGOING_CALL";
+	public final static String OUTGOING_CALL_CLOSE = "OUTGOING_CALL_CLOSE";
+
+	public final static String VIDEOCALL_CLOSE = "VIDEOCALL_CLOSE";
+
+	public final static String CALL_SETUP = CallEvent.CALL_SETUP.toString();
+	public final static String CALL_TERMINATE = CallEvent.CALL_TERMINATE.toString();
+	public final static String CALL_REJECT = CallEvent.CALL_REJECT.toString();
+	public final static String CALL_ERROR = CallEvent.CALL_ERROR.toString();
+	public final static String CALL_CANCEL = CallEvent.CALL_CANCEL.toString();
+}
