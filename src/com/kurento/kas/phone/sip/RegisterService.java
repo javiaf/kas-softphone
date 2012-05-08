d05c30ba100c9bdb661b5fd7312a14f4f7f799f8
Raúl Benítez <rbenitez@gsyc.es>
2012-05-08 14:19:18 +0200

    Add the service class

---
+++ src/com/kurento/kas/phone/sip/RegisterService.java
@@ -0,0 +1,55 @@
+package com.kurento.kas.phone.sip;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.kurento.commons.sip.agent.SipEndPointImpl;
+
+public class RegisterService extends Service {
+
+	private final static String LOG_TAG = RegisterService.class.getName();
+
+	private static SipEndPointImpl endpoint;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// Bundle boundel = intent.getExtras();
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		Log.d(LOG_TAG, "on create method.");
+
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		Log.d(LOG_TAG, "On start method.");
+
+		if (getEndpoint() != null) {
+			int expiresTime = getEndpoint().getExpires();
+			Log.d(LOG_TAG,
+					"Sending register from Second service with expires time  "
+							+ expiresTime);
+			getEndpoint().register();
+		}
+		this.stopSelf();
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d(LOG_TAG, "On destroy method.");
+	}
+
+	public static SipEndPointImpl getEndpoint() {
+		return endpoint;
+	}
+
+	public static void setEndpoint(SipEndPointImpl endpoint) {
+		RegisterService.endpoint = endpoint;
+	}
+
+}
