c59cff482fefdb9e1bd4984835c83906a1577e4d
Raúl Benítez <raulbenitezmejias@gmail.com>
2012-06-06 08:29:38 +0200

    Add new class to implement KurentoUaTimer Modify the service to support the new UaTimer

--- src/com/kurento/kas/phone/sip/RegisterService.java
+++ src/com/kurento/kas/phone/sip/RegisterService.java
@@ -2,17 +2,13 @@ package com.kurento.kas.phone.sip;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-
-import com.kurento.commons.sip.agent.SipEndPointImpl;
-import com.kurento.commons.ua.exception.ServerInternalErrorException;
 
 public class RegisterService extends Service {
 
 	private final static String LOG_TAG = RegisterService.class.getName();
-
-	private static SipEndPointImpl endpoint;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -22,39 +18,27 @@ public class RegisterService extends Service {
 
 	@Override
 	public void onCreate() {
-		Log.d(LOG_TAG, "on create method.");
-
+		Log.d(LOG_TAG, "on create Service.");
 	}
 
 	@Override
 	public void onStart(Intent intent, int startId) {
-		Log.d(LOG_TAG, "On start method.");
+		Bundle b = intent.getExtras();
 
-		if (getEndpoint() != null) {
-			int expiresTime = getEndpoint().getExpires();
-			Log.d(LOG_TAG,
-					"Sending register from Second service with expires time  "
-							+ expiresTime);
+		if (b != null) {
+			Integer uuid = b.getInt("uuid");
+			Log.d(LOG_TAG, "UUID: Service " + uuid);
 			try {
-				getEndpoint().register(expiresTime);
-			} catch (ServerInternalErrorException e) {
-				this.stopSelf();
+				AlarmUaTimer.getTaskTable().get(uuid).run();
+			} catch (Exception e) {
+				// Do nothing
 			}
 		}
-		this.stopSelf();
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(LOG_TAG, "On destroy method.");
-	}
-
-	public static SipEndPointImpl getEndpoint() {
-		return endpoint;
-	}
-
-	public static void setEndpoint(SipEndPointImpl endpoint) {
-		RegisterService.endpoint = endpoint;
 	}
 
 }
